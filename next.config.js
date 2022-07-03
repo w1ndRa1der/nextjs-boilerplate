0ae58e72245887fa7fcfdec236834ab43fed71e3
Joey Ng <joey@joeyng.dev>
2022-07-03 16:05:14 +0800

    Updated basic libraries

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+        dirs: ["src"],
+    },
   reactStrictMode: true,
+
+  // SVGR
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {
+                        typescript: true,
+                        icon: true,
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
 }
 
 module.exports = nextConfig
